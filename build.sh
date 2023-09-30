ad40108a376e3aa4606d342af06648f0370afcd3
kakach <qiancheng.zheng.work@outlook.com>
2023-09-30 14:26:51 +0800

    feat: Add cloud-config and build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+# Make a directory where Repo will be stored and add it to the path
+$ mkdir ~/bin
+$ PATH=~/bin:$PATH
+
+# Download Repo itself
+$ curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+
+# Make Repo executable
+$ chmod a+x ~/bin/repo
